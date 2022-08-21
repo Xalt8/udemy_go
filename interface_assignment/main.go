216db4b1285b520e013b30d2b09da7c1b6349c90
Karan Singh <singh_karan@hotmail.com>
2022-08-21 10:41:29 +0530

    assignment1

--- interface_assignment/main.go
+++ interface_assignment/main.go
@@ -1,5 +1,37 @@
 package main
 
+import "fmt"
+
+type triangle struct {
+	height float64
+	base   float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+type shape interface {
+	getArea() float64
+}
+
+func printArea(s shape) {
+	fmt.Printf("The area is %v\n", s.getArea())
+}
+
 func main() {
 
+	sq := square{sideLength: 10}
+	tr := triangle{height: 10, base: 5}
+
+	printArea(sq)
+	printArea(tr)
 }
